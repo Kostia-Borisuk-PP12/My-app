2c33f7ed9ea2f3ab239e8a8f47e10d177ce03da8
Konstantin <kostya2121555@gmail.com>
2025-04-23 18:17:06 +0300

    text modification

--- com/gmail/Kostia2121555/app.java
+++ com/gmail/Kostia2121555/app.java
@@ -3,6 +3,6 @@ package com.gmail.Kostia2121555;
 
 public class app {
     public static void main(String[] args){
-        System.out.println("This is shop-online app");
+        System.out.println("Shop-online for customers");
     }
 }
