f0fec0f9ab1e1f789c132ca887c1ff95c7caf2b0
Konstantin <kostya2121555@gmail.com>
2025-04-28 19:28:59 +0300

    text modification

--- com/gmail/kostia2121555/AmountSale.java
+++ com/gmail/kostia2121555/AmountSale.java
@@ -1,19 +1,19 @@
-package com.gmail.Kostia2121555;
+package com.gmail.kostia2121555;
 
-public class amount_of_sale {
+public class AmountSale {
     public static void main(String[] args) {
         int productNumber = 1;
         String product = "Product";
         String amount = "total";
         String income = "sales";
         int day = 5;
-        double dayprofit1 = 2400.25;
-        double dayprofit2 = 2500.30;
-        double dayprofit3 = 2399.85;
-        double dayprofit4 = 2451.60;
-        double dayprofit5 = 2401.41;
+        double profitFirstDay = 2400.25;
+        double profitSecondDay = 2500.30;
+        double profitThirdDay = 2399.85;
+        double profitFourthDay = 2451.60;
+        double profitFifthDay = 2401.41;
 
-        double sum = dayprofit1 + dayprofit2 + dayprofit3 + dayprofit4 + dayprofit5;
+        double sum = profitFirstDay + profitSecondDay + profitThirdDay + profitFourthDay + profitFifthDay;
         double average = sum / day;
 
         System.out.println(product + " No " + productNumber + ": smartphone,");
@@ -22,15 +22,15 @@ public class amount_of_sale {
 
         productNumber = 2;
         day = 7;
-        dayprofit1 = 1498.00;
-        dayprofit2 = 1497.90;
-        dayprofit3 = 1498.20;
-        dayprofit4 = 1498.10;
-        dayprofit5 = 1498.25;
-        double dayprofit6 = 1498.15;
-        double dayprofit7 = 1498.25;
+        profitFirstDay = 1498.00;
+        profitSecondDay = 1497.90;
+        profitThirdDay = 1498.20;
+        profitFourthDay = 1498.10;
+        profitFifthDay = 1498.25;
+        double profitSixtyDay = 1498.15;
+        double profitSevenDay = 1498.25;
 
-        sum = dayprofit1 + dayprofit2 + dayprofit3 + dayprofit4 + dayprofit5 + dayprofit6 + dayprofit7;
+        sum = profitFirstDay + profitSecondDay + profitThirdDay + profitFourthDay + profitFifthDay + profitSixtyDay + profitSevenDay;
         average = sum / day;
 
         System.out.println(product + " No " + productNumber + ": laptop,");
