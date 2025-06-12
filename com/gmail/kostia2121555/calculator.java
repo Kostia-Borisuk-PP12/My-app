043700c9d7219d8d9f8df110d4da7d711ffd16ce
Konstantin <kostya2121555@gmail.com>
2025-06-13 00:45:45 +0300

    text modification

---
+++ com/gmail/kostia2121555/calculator.java
@@ -0,0 +1,17 @@
+package com.gmail.kostia2121555;
+
+public class calculator {
+    public double add(double a, double b) {
+        return a + b;
+    }
+    public double subtract(double a, double b) {
+        return a - b;
+    }
+    public double multiply(double a, double b) {
+        return a * b;
+    }
+    public double divide(double a, double b) {
+        if (b == 0) throw new IllegalArgumentException("Cannot divide by zero");
+        return a / b;
+    }
+}
